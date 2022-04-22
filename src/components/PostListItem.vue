80e93049b432c0286cb00b217698e9d9b8f94e97
Abul Hasan <abul.hasan.ict@gmail.com>
2022-04-22 20:36:47 +0600

    Human readable date

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -22,14 +22,15 @@
       </div>
     </div>
 
-    <div class="post-date text-faded">
-      {{ post.publishedAt }}
+    <div class="post-date text-faded" :title="post.publishedAt | humanFriendlyDate">
+      {{ post.publishedAt | diffForHumans }}
     </div>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import moment from 'moment'
 export default {
   props: {
     post: {
@@ -43,6 +44,16 @@ export default {
     },
     userPostCount () {
       return Object.keys(this.user.posts).length
+    }
+  },
+
+  filters: {
+    humanFriendlyDate (date) {
+      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+
+    diffForHumans (date) {
+      return moment.unix(date).fromNow()
     }
   }
 }
