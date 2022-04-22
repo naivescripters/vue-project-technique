657015a198172c0a8cafd02dd543e6554424da12
Abul Hasan <abul.hasan.ict@gmail.com>
2022-04-22 23:43:16 +0600

    Forum Page

---
+++ src/components/AppDate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post-date text-faded" :title="timestamp | humanFriendlyDate">
+    {{ timestamp | diffForHumans }}
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  props: {
+    timestamp: {
+      required: true,
+      type: Number
+    }
+  },
+
+  filters: {
+    humanFriendlyDate (date) {
+      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+
+    diffForHumans (date) {
+      return moment.unix(date).fromNow()
+    }
+  }
+}
+</script>
--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -5,25 +5,25 @@
         <router-link :to="{name: 'ThreadShow', params: {id: thread['.key']}}">{{thread.title}}</router-link>
       </p>
       <p class="text-faded text-xsmall">
-        By <a href="#">{{user.name}}</a>, {{thread.publishedAt}}.
+        By <a href="#">{{user.name}}</a>, <AppDate :timestamp="thread.publishedAt"/>
       </p>
     </div>
 
     <div class="activity">
       <p class="replies-count">{{repliesCount}} replies</p>
 
-      <!-- <img
+      <img
         class="avatar-medium"
         src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png"
         alt=""
-      /> -->
+      />
 
-      <!-- <div>
+      <div>
         <p class="text-xsmall">
           <a href="#">Bruce Wayne</a>
         </p>
-        <p class="text-xsmall text-faded">2 hours ago</p>
-      </div> -->
+        <p class="text-xsmall text-faded"><AppDate :timestamp="thread.publishedAt"/></p>
+      </div>
     </div>
   </div>
 </template>
